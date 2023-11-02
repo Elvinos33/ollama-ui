9dd576a9e142cbd8443d94c25ae3cf6bb35c3e9a
elvinos <elvinos33@duck.com>
2023-11-02 19:40:46 +0100

    initial files and prompt input

---
+++ src/components/UserInput.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  let currentPrompt: string = '';
+</script>
+
+<form class=" input-group flex rounded-container-token w-3/4 ">
+  <input bind:value={currentPrompt} placeholder="Prompt..." class=" flex-1 resize-none bg-transparent border-0 ring-0" />
+  <button class="variant-filled-primary">Send</button>
+</form>
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import UserInput from "../components/UserInput.svelte";
+    import MessageList from "../components/MessageList.svelte"
+
+    let messages: Array<string> = []
+
+</script>
+
+<body class="absolute inset-0">
+  <div class="flex flex-col w-full items-center justify-between">
+    <MessageList />
+    <UserInput />
+  </div>
+</body> 
+
